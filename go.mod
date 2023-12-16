2dd68151ef5f7c056d4a8e34308fff97f5813c20
Jean-Marc Romain <90714406+listonaj@users.noreply.github.com>
2023-12-16 12:31:53 -0800

    used function from dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/listonaj/puppy
 
 go 1.21.5
+
+require github.com/listonaj/GoToDog v0.0.0-20231216201417-d5f07d2e2b11
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	dog "github.com/listonaj/GoToDog"
+)
+
 func Bark() string {
 	return "woof"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
